32c838399101ee9c70687c8d8ca01ea74daccacd
QwertyForce <maximus.bystrov@gmail.com>
2022-04-12 19:58:30 +0300

    ????

--- ClientApp/src/app/components/view-ticket/view-ticket.component.ts
+++ ClientApp/src/app/components/view-ticket/view-ticket.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { BaseModalWindowComponent } from '../base-modal-window/base-modal-window.component';
 
 @Component({
@@ -8,10 +9,26 @@ import { BaseModalWindowComponent } from '../base-modal-window/base-modal-window
 })
 export class ViewTicketComponent extends BaseModalWindowComponent {
 
+  commentForm: FormGroup;
+
   constructor() {
     super();
+    this.commentForm = new FormGroup(
+      {
+        "message": new FormControl("", Validators.required),
+        "user": new FormControl(),
+        "ticket": new FormControl(),
+      }
+    );
   }
 
   ngOnInit(): void {
+  }
+
+  submit(){
+    if (this.commentForm.valid){
+      console.log(this.commentForm);
+      
+    }
   }
 }
