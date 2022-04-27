8e15d9102fc2fede69ac7aa5cc55153e72e2bd35
QwertyForce <maximus.bystrov@gmail.com>
2022-04-27 15:57:18 +0300

    project build

--- docs/main.js
+++ docs/main.js
@@ -1917,7 +1917,7 @@ class BoardComponent {
     }
     applyFilters(ticket) {
         const [type, assignedTo, priority] = [
-            this.filters.type.length === 0 ? true : this.filters.type.some(t => t === ticket.type),
+            this.filters.type.length === 0 ? true : this.filters.type.some(t => t === ticket.type || t === ""),
             this.filters.assignedTo.length === 0 ? true : this.filters.assignedTo.some(t => +t === ticket.assignedTo.id || +t === 0),
             this.filters.priority.length === 0 ? true : this.filters.priority.some(t => +t === ticket.priority.id || +t === 0)
         ];
